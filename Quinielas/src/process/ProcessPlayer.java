082a016a22564712e4e24ea0811946f5350c15e8
corono <corono.fj@gmail.com>
2014-08-25 00:27:33 +0200

    added streak factor and players value

--- Quinielas/src/process/ProcessPlayer.java
+++ Quinielas/src/process/ProcessPlayer.java
@@ -12,10 +12,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import static file.MatchsFile.*;
+
 /**
  *
  * @author francisco
  */
+        
 public class ProcessPlayer {
     /**
      * extract the players from a season and evaluates their team influence
@@ -35,6 +38,7 @@ public class ProcessPlayer {
         Statement st = conexion.createStatement();
         ResultSet result = st.executeQuery(query);
         while (result.next()){
+            
             if(result.getInt("PJugados") == 0){
                 mxp = 0;
             }else{
@@ -50,11 +54,10 @@ public class ProcessPlayer {
             aux5 = result.getInt("Goles") * result.getInt("PJugados");
             //System.out.println(Math.sin(360/6)+" "+aux0 + " "+aux1 + " "+aux2 + " "+aux3 + " "+aux4 + " "+aux5 + " ");
                 
-            tmp = result.getString("Temporada");
             val = 0.5 * Math.sin(360/n) * (aux0 + aux1 + aux2 + aux3 + aux4 + aux5)* -1;
             try{
                 Statement st2 = conexion.createStatement();
-                String query2 = "UPDATE LIGA.Historico_Jugador SET ValPos = '" + val + "' WHERE Jugadores_idJugadores = '" + result.getString("Jugadores_idJugadores") + "' AND Temporada = '" + tmp +"'";
+                String query2 = "UPDATE LIGA.Historico_Jugador SET ValPos = '" + val + "' WHERE Jugadores_idJugadores = '" + result.getString("Jugadores_idJugadores") + "' AND Temporada = '" + result.getString("Temporada") +"'";
                 //System.out.println(query2);
                 st2.executeUpdate(query2);
             }
@@ -67,11 +70,39 @@ public class ProcessPlayer {
             
             
         }
-    }    
+    }
+    
+    public static void idTeamHistorico() throws Exception{
+        
+        Connection conexion = getConexionBBDD();
+        
+        String query =  "SELECT * FROM LIGA.Historico_Jugador";
+        Statement st = conexion.createStatement();
+        ResultSet result = st.executeQuery(query);
+        
+        while (result.next()){
+            try{
+                Statement st2 = conexion.createStatement();
+                String query2 = "UPDATE LIGA.Historico_Jugador SET idEquipo = " + getTeamID(result.getString("Equipo")) + " WHERE Jugadores_idJugadores = '" + result.getString("Jugadores_idJugadores") + "' AND Temporada = '" + result.getString("Temporada") +"'";
+                System.out.println(query2 + " "+ result.getString("Equipo"));
+                st2.executeUpdate(query2);
+            }
+            catch (SQLException e){
+                System.out.println (e.getMessage());
+                e.printStackTrace();
+                break;
+                
+            }
+            
+        }
+        
+        
+    }
    
     
     public static void main(String[] args) throws Exception {
-        evaluationTeamPlayer();
+        //evaluationTeamPlayer();
+        idTeamHistorico();
     }
     
     
--- Quinielas/src/process/ProcessSeason.java
+++ Quinielas/src/process/ProcessSeason.java
@@ -61,8 +61,8 @@ public class ProcessSeason {
         int[] vector = new int[6];
         Integer[] localData = new Integer[17];
         Integer[] visitData = new Integer[17];
-        double[] entrada = new double[24];
-        Integer local, visitante;
+        double[] entrada = new double[26];
+        Integer local, visitante, streak = 5;
         String  temp;
         
         while (result.next()){
@@ -106,7 +106,7 @@ public class ProcessSeason {
             visitData[15] = result.getInt("VisitanteGCV");
             visitData[16] = result.getInt("VisitantePuntos");
           
-            
+            System.out.println(result.getString("Temporada")+" "+result.getInt("Jornada")+" "+ local+" "+visitante);
             vector = getMatchesBetween(local, visitante, temp, conexion);
             
             entrada[0] = (double)local;
@@ -137,26 +137,27 @@ public class ProcessSeason {
                 entrada[12] = (double)(localData[12] - localData[13]) / (localData[12] + localData[13]); //promedio diferencia goles partidos como local
             }else{
                 entrada[12] = 0.0;
-            }
+            }            
             entrada[13] = localData[16] / localData[0]; //promedio puntos obtenidos por partido
+            entrada[14] = (double)streak(conexion, local,streak,result.getInt("Jornada"), result.getString("Temporada"));
             
-            entrada[14] = (double)visitData[1] / visitData[0]; //porcentaje partidos ganados hasta la fecha
-            entrada[15] = (double)visitData[7] / (visitData[7] + visitData[8] + visitData[9]); //Porcentaje partidos ganados como visitante hasta la fecha
-            entrada[16] = (double)visitData[8] / (visitData[7] + visitData[8] + visitData[9]); //porcentaje partidos empatados como visitante hasta la fecha
-            entrada[17] = (double)visitData[9] / (visitData[7] + visitData[8] + visitData[9]); //porcentaje partidos perdidos como visitante hata la fecha
+            entrada[15] = (double)visitData[1] / visitData[0]; //porcentaje partidos ganados hasta la fecha
+            entrada[16] = (double)visitData[7] / (visitData[7] + visitData[8] + visitData[9]); //Porcentaje partidos ganados como visitante hasta la fecha
+            entrada[17] = (double)visitData[8] / (visitData[7] + visitData[8] + visitData[9]); //porcentaje partidos empatados como visitante hasta la fecha
+            entrada[18] = (double)visitData[9] / (visitData[7] + visitData[8] + visitData[9]); //porcentaje partidos perdidos como visitante hata la fecha
             if((visitData[10] + visitData[11])>0){
-                entrada[18] = (double)(visitData[10] - visitData[11]) / (visitData[10] + visitData[11]); //promedio difrenecia goles hasta la fecha
-            }else{
-                entrada[18] = 0.0;
-            }
-            
-            if((visitData[12] + visitData[13])>0){
-                entrada[19] = (double)(visitData[12] - visitData[13]) / (visitData[12] + visitData[13]); //promedio diferencia goles como visitante
+                entrada[19] = (double)(visitData[10] - visitData[11]) / (visitData[10] + visitData[11]); //promedio difrenecia goles hasta la fecha
             }else{
                 entrada[19] = 0.0;
             }
-            entrada[20] = (double)visitData[16] / visitData[0]; //promedio puntos por partido
             
+            if((visitData[12] + visitData[13])>0){
+                entrada[20] = (double)(visitData[12] - visitData[13]) / (visitData[12] + visitData[13]); //promedio diferencia goles como visitante
+            }else{
+                entrada[20] = 0.0;
+            }
+            entrada[21] = (double)visitData[16] / visitData[0]; //promedio puntos por partido
+            entrada[22] = (double)streak(conexion, visitante, streak, result.getInt("Jornada"), result.getString("Temporada"));
            
 //            if(result.getBoolean("VL")){
 //                entrada[21] = 0.0;
@@ -180,8 +181,8 @@ public class ProcessSeason {
 //                pw.print(entrada[i] +", ");
 //            }
 //             pw.print(entrada[entrada.length-3]+"\n");
-             System.out.println(result.getString("Temporada")+" "+result.getInt("Jornada")+" : ");
-             for(int i = 0; i<21; i++){                
+             
+             for(int i = 0; i<23; i++){                
                 pw.print(entrada[i] +", ");
             }
              if(result.getBoolean("VL")){
@@ -279,10 +280,95 @@ public class ProcessSeason {
         
 
     }
+    /**
+     * return the streak of a team in a determined season 
+     * @param conexion
+     * @param equipo
+     * @param racha
+     * @param jornada
+     * @param temporada
+     * @return
+     * @throws Exception 
+     */
+    public static int streak(Connection conexion, int equipo, int racha, int jornada, String temporada) throws Exception{
+        
+            int ji;
+            Statement st = conexion.createStatement();
+
+            if (jornada <= racha){
+                ResultSet result = st.executeQuery("SELECT * FROM LIGA.Clasificacion WHERE Jornada in (1,"+jornada+") AND Equipos_idEquipos = "+equipo+" AND Temporada = '"+temporada+"'");
+                return processRacha(result,1,jornada);
+               
+                
+            }else{
+                ji = jornada-racha;
+                ResultSet result = st.executeQuery("SELECT * FROM LIGA.Clasificacion WHERE Jornada in ("+ji+","+jornada+") AND Equipos_idEquipos = "+equipo+" AND Temporada = '"+temporada+"' ORDER BY Jornada DESC");
+                return processRacha(result,ji,jornada);
+            }
+            
+        
+    }
+    /**
+     * returns the streak of the team in the last matches
+     * @param rs
+     * @param ji 
+     * @param jf
+     * @return
+     * @throws SQLException 
+     */
+    public static int processRacha (ResultSet rs, int ji, int jf) throws SQLException{
+        
+        int racha = 0;
+        ArrayList<Integer> vector = new ArrayList<>();      
+        System.out.println(rs.next());
+            
+            while (rs.next()) {
+                vector.add(rs.getInt("PG"));
+                vector.add(rs.getInt("PP"));
+                               
+            }
+            if(vector.isEmpty()){
+                racha = 0;
+            }
+            else if(vector.size() > 2){
+                racha = (vector.get(0) - vector.get(2)) - (vector.get(1) - vector.get(3));
+            }else {
+                racha = vector.get(0)-vector.get(1);
+            }
+            rs.close();
+          
+            return racha;
+    }
+    /**
+     * returns the roaster value for a team in a season, and the number of players 
+     * @param conexion
+     * @param teamID 
+     * @param temporada 
+     * @return  
+     * @throws java.sql.SQLException 
+     */
+    public static double[] addPlayersValue(Connection conexion, int teamID, String temporada ) throws SQLException{
+            
+            double totalValue = 0, numJugadores = 0;
+            double[] vector = null;
+            
+            Statement st = conexion.createStatement();
+            ResultSet result = st.executeQuery("SELECT * FROM LIGA.Historico_Jugador h JOIN Equipos e ON e.idEquipos = h.idEquipo WHERE e.idEquipos = "+ teamID + " AND h.Temporada = '"+ temporada +"'");
+                        
+            while (result.next()){
+                numJugadores ++;
+                totalValue += result.getDouble("h.ValPos");
+            }
+            vector[0] = totalValue;
+            vector[1] = numJugadores;
+            
+            return vector;
+    }
+    
     public static void main(String[] args) throws Exception {
         
         ArrayList<String> seasons = getSeasons();
-        FileWriter fichero = new FileWriter("/home/francisco/Dropbox/TFG/data/wekafiles/train_V.csv");
+        FileWriter fichero = new FileWriter("/home/francisco/Dropbox/TFG/data/wekafiles/train_borr.csv");
         PrintWriter pw = new PrintWriter(fichero);
         
         //train
@@ -296,8 +382,9 @@ public class ProcessSeason {
         pw.flush();
         pw.close();
         
+        System.exit(0);
         
-        FileWriter ficheroTest = new FileWriter("/home/francisco/Dropbox/TFG/data/wekafiles/test_V.csv");
+        FileWriter ficheroTest = new FileWriter("/home/francisco/Dropbox/TFG/data/wekafiles/test_borr.csv");
         PrintWriter pwtest = new PrintWriter(ficheroTest);
         //test
         for(int i = seasons.size()-5; i < seasons.size()-1; i++){
